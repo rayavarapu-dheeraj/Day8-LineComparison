85a2c261e2b935682118e224a2fb7b1a7af90a4a
rayavarapu-dheeraj <rayavarapu059@gmail.com>
2021-08-05 16:42:16 +0530

    added: Length of the line :

--- src/com/bridgelabz/lineComparison/LineComp.java
+++ src/com/bridgelabz/lineComparison/LineComp.java
@@ -1,9 +1,22 @@
 package com.bridgelabz.lineComparison;
 
+import java.util.Scanner;
+
 public class LineComp {
 
+	static Scanner sc =new Scanner(System.in);
 	public static void main(String[] args) {
-		System.out.println("Welcome to line Comparison");
+		System.out.println("Welcome to line Comparison:");
+		System.out.println("Enter the value of co-ordinate:");
+		System.out.print("Enter the value of (x1,y1)= :");
+		int x1 = sc.nextInt();
+		int y1 = sc.nextInt();
+		System.out.print("Enter the value of (x2,y2)= :");
+		int x2 = sc.nextInt();
+		int y2 = sc.nextInt();
+
+		double lengthOfLine1 = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+		System.out.println("the length of the line is "+lengthOfLine1);
 	}
 
 }
