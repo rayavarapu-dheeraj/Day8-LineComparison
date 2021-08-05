6b4aa60838940bc972f19e9f419c2094312f15c3
rayavarapu-dheeraj <rayavarapu059@gmail.com>
2021-08-05 17:11:51 +0530

    added: Length of the line:

--- src/com/bridgelabz/lineComparison/LineComp.java
+++ src/com/bridgelabz/lineComparison/LineComp.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 
 public class LineComp {
 
-	static Scanner sc =new Scanner(System.in);
+	static Scanner sc = new Scanner(System.in);
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to line Comparison:");
 		System.out.println("Enter the value of co-ordinate:");
@@ -16,7 +17,24 @@ public class LineComp {
 		int y2 = sc.nextInt();
 
 		double lengthOfLine1 = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-		System.out.println("the length of the line is "+lengthOfLine1);
-	}
 
+		System.out.println("Enter the values of next co-ordinates :");
+		System.out.print("Enter the value of (x3,y3)= ");
+		int x3 = sc.nextInt();
+		int y3 = sc.nextInt();
+		System.out.print("Enter the value of (x4,y4)= ");
+		int x4 = sc.nextInt();
+		int y4 = sc.nextInt();
+
+		double lengthOfLine2 = Math.sqrt(Math.pow(x4 - x3, 2) + Math.pow(y4 - y3, 2));
+
+		System.out.println("Length of Line 1= " + lengthOfLine1);
+		System.out.println("Length of Line2= " + lengthOfLine2);
+
+		if (lengthOfLine1 == lengthOfLine2) {
+			System.out.println("Two Lines are Equal");
+		} else {
+			System.out.println("Two Lines are Not Equal");
+		}
+	}
 }
